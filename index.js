2e5e8845d87021f9b346c0986e62515a4ea50346
Martinpouh <martinchege2015@gmail.com>
2022-06-03 08:27:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+let thisArray = [];
+
+function writeCards(cards) {
+    for (let i = 0; i < cards.length; i++) {
+        thisArray.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    }
+    return thisArray
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer);
+        integer--;
+    }
+}
+countDown(4);
